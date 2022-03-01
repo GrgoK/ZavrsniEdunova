c95e70a22c7ac0d2fc1e430b350a2117e8c9d5e6
GrgoK <93468203+GrgoK@users.noreply.github.com>
2022-03-01 16:42:43 +0100

    jos malo popravljanja

--- app/core/App.php
+++ app/core/App.php
@@ -4,55 +4,65 @@ class App
 {
     public static function start()
     {
+        //echo '<pre>';
+        //print_r($_SERVER);
+        //echo '</pre>';
         $ruta = Request::getRuta();
+        //echo $ruta;
+
+        $djelovi = explode('/',$ruta);
         
-        $djelovi = explode('/', $ruta);
+        //echo '<pre>';
+        //print_r($djelovi);
+        //echo '</pre>';
 
         $klasa='';
         if(!isset($djelovi[1]) || $djelovi[1]===''){
-            $klasa='index';
+            $klasa='Index';
         }else{
             $klasa=ucfirst($djelovi[1]);
-
         }
-
         $klasa .= 'Controller';
+        //echo $klasa;
 
         $metoda='';
         if(!isset($djelovi[2]) || $djelovi[2]===''){
             $metoda='index';
-        } else{
+        }else{
             $metoda=$djelovi[2];
         }
 
-        if(class_exits($klasa) && method_exists($klasa,$metoda)){
+        //echo $klasa . '->' . $metoda . '()';
+
+        if(class_exists($klasa) && method_exists($klasa,$metoda)){
+            // klasa i metoda postoje, instancirati klasu i pozvati metodu
             $instanca = new $klasa();
             $instanca->$metoda();
         }else{
+            // metoda na klasi ne postoji, obavijestiti korisnika
             $view = new View();
-            $view->render('eror404',[
+            $view->render('error404',[
                 'onoceganema' =>$klasa . '->' . $metoda
             ]);
+            //echo $klasa . '->' . $metoda . '() ne postoji';
         }
 
+        //$kontroler = new IndexController();
+        //$kontroler->index();
+
     }
 
-
-public static function config($kljuc)
-{
-    $config = include BP_APP . 'konfiguracija.php';
-    return $config[$kljuc];
-     }
-
-   public static function autoriziran()
- {
-    if(isset($_SESSION) && isset($_SESSION['autoriziran'])){
-        return true;
+    public static function config($kljuc)
+    {
+        $config = include BP_APP . 'konfiguracija.php';
+        return $config[$kljuc];
     }
+    public static function autoriziran()
+    {
+        if(isset($_SESSION) && isset($_SESSION['autoriziran'])){
+            return true;
+        }
 
-    return false;
- }
-
-
+        return false;
+    }
 }
-
--- app/core/View.php
+++ app/core/View.php
@@ -1,20 +1,19 @@
 <?php
 
-class View 
+class View
 {
     private $predlozak;
-    
+
     public function __construct($predlozak='predlozak')
     {
         $this->predlozak=$predlozak;
-
     }
 
     public function render($phtmlStranica,$parametri=[])
     {
         ob_start();
-        extract($parametri);
-        include_once BP_APP . 'view' . DIRECTORY_SEPARATOR . $phtmlStranica . '.phmtl';
+        extract($parametri); // od asocijativnog niza kreira varijable
+        include_once BP_APP . 'view' . DIRECTORY_SEPARATOR . $phtmlStranica . '.phtml';
         $sadrzaj = ob_get_clean();
         include_once BP_APP . 'view' .  DIRECTORY_SEPARATOR . $this->predlozak . '.phtml';
     }
